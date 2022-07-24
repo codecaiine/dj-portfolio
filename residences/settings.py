1bb7d1bbb607796c22163865a6ea01f04843b07c
codecaiine <akabrouyannickn@gmail.com>
2022-07-24 18:00:52 +0000

    Generate static files

--- residences/settings.py
+++ residences/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATICFILES_DIRS = [BASE_DIR / "residences/static"]  
 STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles" 
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  
 
